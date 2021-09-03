b06fddb60971c0bff3506bc6a6522d96a0955486
Vishal Agrahari <vishalagrahari19@gmail.com>
2021-09-03 17:15:17 +0530

    Committing 2 files

--- src/org/gittest/practice/HelloGit1.java
+++ src/org/gittest/practice/HelloGit1.java
@@ -1,11 +1,11 @@
 package org.gittest.practice;
 
-public class HelloGit {
+public class HelloGit1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Vishal!!");
+		System.out.println("Hello Git1!!");
 
 	}
 
