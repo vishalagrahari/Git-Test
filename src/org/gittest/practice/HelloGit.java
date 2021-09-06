a9f55bc5cb2a35894ddb3abb428b893b9cd9b4a6
Vishal Agrahari <vishalagrahari19@gmail.com>
2021-09-06 16:00:11 +0530

    First commit

--- src/org/gittest/practice/HelloGit.java
+++ src/org/gittest/practice/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Vishal!!");
+		System.out.println("Hello Vishal. Welcome!!");
 
 	}
 
--- src/org/gittest/practice/HelloGit1.java
+++ src/org/gittest/practice/HelloGit1.java
@@ -5,7 +5,7 @@ public class HelloGit1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Git1!!");
+		System.out.println("Hello Git1. My git test project!!");
 
 	}
 
